d02dccd51cc0fadcce6798d5cb8059be42845589
Jihani-31 <jihaninanayakkara@gmail.com>
2021-07-04 14:06:59 +0530

    menue item creare-use material ui

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import {FormControl,Select,MenuItem} from '@material-ui/core';
 
 function App() {
+  const[countries,setCountries]=useState(['worldWide','SriLanka','India','UK'])
   return (
-    <div className="App">
-     <h1>Covid19</h1>
+    <div className="app">
+      <div className="app_header">
+      <h1>Covid19</h1>
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value="dummy">
+          {countries.map(country=><MenuItem value={country}>{country}</MenuItem>)}
+        </Select>
+      </FormControl>
+      </div>  
     </div>
   );
 }
