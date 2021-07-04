d63f50a18fc760005679476c3917da75e0f73fda
Jihani-31 <jihaninanayakkara@gmail.com>
2021-07-04 20:43:39 +0530

    create info box

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,9 +1,11 @@
 import { useState,useEffect} from 'react';
 import {FormControl,Select,MenuItem} from '@material-ui/core';
+import InfoBox from './components/InfoBox';
 import './App.css';
 
 function App() {
   const[countries,setCountries]=useState([]);  //how to write variable in reacct -useState
+  const[country,setCountry]=useState('worldwide');
   useEffect(()=>{                          //runs a piece of code,based on given condition
     //the code inside here will run once
     //when the component loads and not again
@@ -25,18 +27,29 @@ function App() {
     getCountriesData()
   },[]);
 
+  const onCountryChange=(event) =>{
+    const countryCode=event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app_header">
-      <h1>Covid19</h1>
+      <h1>Covid-19</h1>
       <FormControl className="app_dropdown">
-        <Select variant="outlined" value="dummy">
+        <Select variant="outlined" value={country} onChange={onCountryChange}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
           {countries.map(country=>
           <MenuItem value={country.value}>{country.name}</MenuItem>)
           }
         </Select>
       </FormControl>
       </div>  
+      <div className="app_status">
+        <InfoBox title="Coronavirus Cases" total={4000}  cases={500}/>
+        <InfoBox title="Recovered"  total={700} cases={4000}/>
+        <InfoBox title="Deaths" total={300} cases={3000}/>
+      </div>
     </div>
   );
 }
